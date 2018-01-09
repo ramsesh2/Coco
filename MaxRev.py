fd9a89aa62f53c66b144df7b9e704dca51b27367
Ram Muthukumar <ram_muthukumar@utexas.edu>
2018-01-08 18:46:23 -0600

    beautification

--- MaxRev.py
+++ MaxRev.py
@@ -1,3 +1,23 @@
+'''
+To select the companies to sell to in order to achieve maximum revenue
+
+Assumptions made:
+
+1) Orders can only be completed in full. If a company requests 5 items at a 
+	price, then you can only sell them exactly 5.
+2) The price given is the price the company is willing to pay for the 
+	total quantity given, NOT the per unity price.
+3) I am sticking to built in python data structures (dictionaries and lists, 
+	but a better approach would be a dataframe or database to keep the data
+	tabular. I did not use those to keep the solution simple and portable.
+
+usage:
+python MaxRev.py [Supply of product available]
+
+The customer order amounts and prices MUST reside in a file called
+Orders.csv, each column delimited by a comma
+'''
+
 import argparse
 import csv
 
@@ -26,18 +46,25 @@ with open('Orders.csv', 'r') as orders:
 
 best = []
 
-
+#dynamic programming recursive function
 def pick(stock, comps):
-	print stock
+#	print stock
+
+#if there are no more companies to look at or if there is no more supply
 	if not comps:
 		return 0
 	if stock == 0:
 		return 0
+
+#pop the last element in the list of companies
 	last = comps.pop()
 
+#if there is not enough stock to fill the order, move on to the next one
 	if stock - quantity[last] < 0:
 		return pick(stock, comps)
 
+#either choose the company or don't, build a tree of each of the possible combinations of companies
+#whose orders will fit. Choose the path with the highest revenue in the end.
 	else:
 		sel = price[last] + pick(stock-quantity[last], comps)
 		notsel = pick(stock, comps)
@@ -47,5 +74,12 @@ def pick(stock, comps):
 		else:
 			return notsel
 
-print pick(supply, companies)
-print best
+rev = pick(supply, companies)
+#print best
+
+print 'Should complete the following orders:'
+quant = 0
+for s in best:
+	print 'Company: ', s, '   Amount: ', quantity[s],'   Price: ', price[s]
+	quant += quantity[s]
+print 'Total Quantity: ', quant, 'Total Revenue: ', rev
