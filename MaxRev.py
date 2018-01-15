635456c782037416874cdc501348082b0b4cf1f9
Ram Muthukumar <ram_muthukumar@utexas.edu>
2018-01-15 12:04:00 -0600

    finally got a working solution

--- MaxRev.py
+++ MaxRev.py
@@ -44,42 +44,69 @@ with open('Orders.csv', 'r') as orders:
 		price[row[0]] = int(row[2])
 		companies.append(row[0])
 
-best = []
+best = [[0 for x in range(supply + 1)] for y in range(len(companies) + 1)]
+keep = [[0 for x in range(supply + 1)] for y in range(len(companies) + 1)]
+take = []
 
 #dynamic programming recursive function
-def pick(stock, comps):
+def pick(stock, comps, n):
 #	print stock
+#	print comps
 
+	for i in range(1, n+1):
+		for w in range(stock+1):
+#			print i, w
+			if(quantity[comps[i-1]] <= w and (price[comps[i-1]] + best[i-1][w-quantity[comps[i-1]]] > best[i-1][w])):
+				best[i][w] = price[comps[i-1]] + best[i-1][w-quantity[comps[i-1]]]
+				keep[i][w] = 1
+			else:
+				best[i][w] = best[i-1][w]
+				keep[i][w] = 0
+
+#	print best
+#	print keep
+	temp = supply
+	for i in range(n, 0, -1):
+		if(keep[i][temp] == 1):
+			take.append(comps[i-1])
+			temp = temp - quantity[comps[i-1]]
+	return best[n][supply]
+
+'''
 #if there are no more companies to look at or if there is no more supply
-	if not comps:
+	if n==0:
 		return 0
 	if stock == 0:
 		return 0
 
 #pop the last element in the list of companies
-	last = comps.pop()
+	last = comps[n-1]
 
 #if there is not enough stock to fill the order, move on to the next one
 	if stock - quantity[last] < 0:
-		return pick(stock, comps)
+		return pick(stock, comps, n-1)
 
 #either choose the company or don't, build a tree of each of the possible combinations of companies
 #whose orders will fit. Choose the path with the highest revenue in the end.
 	else:
-		sel = price[last] + pick(stock-quantity[last], comps)
-		notsel = pick(stock, comps)
+		sel = price[last] + pick(stock-quantity[last], comps, n-1)
+		notsel = pick(stock, comps, n-1)
 		if(sel > notsel):
-			best.append(last)
+			best[stock][n] = last
 			return sel
 		else:
+			best[stock][n] = last
 			return notsel
 
-rev = pick(supply, companies)
-#print best
+'''
 
+n = len(companies)
+rev = pick(supply, companies, n)
+#print best
+#print take
 print 'Should complete the following orders:'
 quant = 0
-for s in best:
+for s in take:
 	print 'Company: ', s, '   Amount: ', quantity[s],'   Price: ', price[s]
 	quant += quantity[s]
 print 'Total Quantity: ', quant, 'Total Revenue: ', rev
